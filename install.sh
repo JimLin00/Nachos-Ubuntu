3890b6c2c6b948572348e43eba4c1191d53dc651
JimLin00 <jimlin9109@gmail.com>
2024-10-11 23:22:55 +0800

    Ubuntu Verison

---
+++ install.sh
@@ -0,0 +1,25 @@
+#install all the requirements
+sudo apt update
+sudo apt-get install build-essential
+
+sudo dpkg --add-architecture i386
+
+sudo apt-get install csh
+
+sudo apt-get install gcc
+
+sudo apt-get install g++
+
+sudo apt-get update
+
+sudo apt-get install zlib1g:i386 libstdc++6:i386
+
+sudo apt-get install libc6:i386 libncurses5:i386
+
+sudo apt-get install libgcc1:i386 libstdc++5:i386
+
+#install cross compiler
+
+tar zxvf mips-decstation.linux-xgcc.gz
+mv usr/local/nachos /usr/local
+rm -r usr
